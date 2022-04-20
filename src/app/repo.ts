32cb105e58b3a038a961468d7879e63a5784b8f0
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-20 07:35:07 +0300

    add repo properties

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,2 +1,4 @@
-export class Repo {
+export class Repos {
+  constructor(public name:string,public html_url:string ,public description:string, public created_at:Date, public forks:number,public watchers_count:number, public language:string){
+  }
 }
