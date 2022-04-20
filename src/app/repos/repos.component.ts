c8dcf43a9a39147cf1022dfb2016ca232fab10b8
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-20 07:58:22 +0300

    modify the repo component

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-repos',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReposComponent implements OnInit {
 
-  constructor() { }
+ // public repo:any=[];
+
+  constructor(private apiService: ApiService) { 
+    // this.apiService.getRepo().subscribe((response: any) => {
+    //   this.repo = response;
+    //   console.log(response);
+      
+    // })
+  }
 
   ngOnInit(): void {
   }
-
+ 
 }
