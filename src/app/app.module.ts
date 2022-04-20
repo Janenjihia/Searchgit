3f5b499642cb8abbf02e0802726ea84f7e4d8ec8
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-20 08:13:23 +0300

    make final changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ReposComponent } from './repos/repos.component';
 import { UsersComponent } from './users/users.component';
@@ -11,15 +10,15 @@ import { DatePipePipe } from './date-pipe.pipe';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from './api.service';
-
+import { formComponent } from './form/form.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
     NavbarComponent,
     ReposComponent,
     UsersComponent,
-    DatePipePipe
+    DatePipePipe,
+    formComponent,
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey:"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"
 };
