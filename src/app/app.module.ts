04ceba18a2fc102dbcdd40dfbfc9c71a31a26b1d
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-20 07:27:52 +0300

    add form, navbar, repos and users component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ReposComponent } from './repos/repos.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    NavbarComponent,
+    ReposComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
