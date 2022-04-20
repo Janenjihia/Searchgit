9637bc1612f1dd8a08f9bf3c60668577fd090d3c
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-20 07:55:43 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { reposComponent } from './repos/repos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"users", component:UsersComponent},
+  {path:"repos", component: reposComponent},
+  // {path: "", redirectTo:"/user-profile", pathMatch:"full"},
+  // {path:"**",component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
