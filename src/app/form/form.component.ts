66305fcdb8ee3600763efa10174d7a5d13439db4
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-20 08:01:44 +0300

    edit form component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
+export class formComponent implements OnInit {
+  // add intializer as quick fix
+  // searchName!: string;
+  // user! : string;
+  // repo!: string
+  // @Output() searchOutput = new EventEmitter<any>()
+  constructor(private apiservice: ApiService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
   }
+  // search(){
+  //   this.apiservice.getRepo().subscribe((response:any)=>{
+  //     this.repo=response
+  //   })
+  //   this.apiservice.getUserInfo().subscribe((response:any)=>{
+  //     this.user=response
+  //   })
+  //   // this.searchOutput.emit(this.user);
+  //   // this.user = "";
+  // }
 
 }
