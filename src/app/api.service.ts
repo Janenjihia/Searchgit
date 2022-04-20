be870f4daefe8e9cd9a9b572238ba8df3b0caaac
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-20 07:36:48 +0300

    define service api

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,33 @@
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
+import { map } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private userName: string;
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) { 
+    
+    this.userName = "janenjihia"
+  }
+
+  getUserInfo() {
+    return this.http.get("https://api.github.com/users/" + this.userName).pipe(map(data => {
+      return data;
+    }));
+  }
+  getRepo() {
+    return this.http.get("https://api.github.com/users/" + this.userName + "/repos?api_key=" + environment.apiKey).pipe(map(data => {
+      return data
+    }));
+  }
+  getUserName (userName:string){
+    this.userName=userName
+  }
+  
+  }
